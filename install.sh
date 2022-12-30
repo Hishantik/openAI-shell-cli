44655333169a243ca1d30056b2407cba59b4b7f6
Hishantik <rishabhsharmah29@gmail.com>
2022-12-30 22:22:53 +0530

    update

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ curl -sS https://raw.githubusercontent.com/Hishantik/openAI-shell-cli/main/dekua
 
 chmod +x ~/../usr/bin/dekuai
 
-read -n "Please enter your OpenAI API key: " token 
+read -n "Please enter your OpenAI API key: "  
 
 # Adding OpenAI token to shell profile
 if [ -f ~/.zshrc ]; then
